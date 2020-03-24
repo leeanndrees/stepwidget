a8493bdcd7b3ed3ac0a32ae9dff21a905da685a5
Leeann Drees <leeann.drees@detroitlabs.com>
2020-03-24 11:11:46 -0400

    Initial commit

---
+++ StepWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StepWidget
+//
+//  Created by Leeann Drees on 3/24/20.
+//  Copyright © 2020 DetroitLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
